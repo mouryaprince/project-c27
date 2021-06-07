f126f825c7c81844f0515df10414313060fa769b
mouryaprince <82656217+mouryaprince@users.noreply.github.com>
2021-06-07 19:40:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-c27
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ rope.js
@@ -0,0 +1,31 @@
+class Rope{
+    constructor(body1,body2,offsetX,offsetY){
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+
+        var options = {
+            bodyA:body1,
+            bodyB:body2,
+            pointB: {x:this.offsetX, y:this.offsetY},
+        }
+          
+        this.rope = Matter.Constraint.create(options);
+        World.add(world,this.rope);
+    }
+    display(){
+        var point1 = this.rope.bodyA.position;
+        var point2 = this.rope.bodyB.position;
+
+        strokeWeight(2);
+
+        var Anchoe1X = point1.x;
+        var Anchoe1Y = point2.y;
+
+
+        var Anchoe2X = point2.x +this.offsetX;
+        var Anchoe2Y = point2.y +this.offsetY;
+
+        line(Anchoe1X, Anchoe1Y, Anchoe2X, Anchoe2Y)
+
+    }
+}
